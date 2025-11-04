4d9d48171ef5bbd68bcb6ee6a9f8919b845cd8dc
Arkapravo <arkapravochakrabarti188@gmail.com>
2025-11-05 01:28:42 +0530

    Next-Props

--- 02-components/eslint.config.js
+++ 02-components/eslint.config.js
@@ -24,14 +24,29 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
+      // base recommended rules
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+
+      // your custom overrides / turns-offs
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+
+      // OVERRIDE no-unused-vars so `React` is ignored (prevents "declared but never used")
+      'no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+          argsIgnorePattern: '^_',       // allow unused args starting with _
+          varsIgnorePattern: '^React$',  // ignore variable named "React"
+        },
       ],
     },
   },
--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Card from "./components/Card";
+import NavBar from "./components/NavBar";
 
 const App = () => {
+  const user = "Sarthak";
+  const age = 89;
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <div>
+        <NavBar />
+        <NavBar />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
