ac0fff40c9dbd72e4ac843b0f24b66b87cacf7ef
Arkapravo <arkapravochakrabarti188@gmail.com>
2025-11-05 20:14:42 +0530

    1.47

--- 03-props/eslint.config.js
+++ 03-props/eslint.config.js
@@ -28,11 +28,15 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+
+      // ✅ ignore unused variables
+      'no-unused-vars': 'off',
     },
   },
 ]
--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Card from "./components/Card";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="parent">
+      <Card
+        user="Aman Singh"
+        age={18}
+        img={
+          "https://plus.unsplash.com/premium_photo-1690571200236-0f9098fc6ca9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3BhY2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500"
+        }
+      />
+      <Card
+        user="Sarthak"
+        age={21}
+        img={
+          "https://plus.unsplash.com/premium_photo-1761940415449-c09ef466c698?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=500"
+        }
+      />
+      <Card
+        user="Aditya"
+        age={24}
+        img={
+          "https://plus.unsplash.com/premium_vector-1711987726767-a73708a2d3de?ixlib=rb-4.1.0&auto=format&fit=crop&q=60&w=352&dpr=1&h=367"
+        }
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
