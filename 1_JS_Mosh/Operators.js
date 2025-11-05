3c79598f070758fa9d00d25bcc3bbbec198fdfa0
Arkapravo <arkapravochakrabarti188@gmail.com>
2025-11-06 01:10:00 +0530

    20

---
+++ 1_JS_Mosh/Operators.js
@@ -0,0 +1,17 @@
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+console.log(x++);
+console.log(--x);
+
+let a = 1;
+console.log(x < 0);
+
+console.log(1=='1')
