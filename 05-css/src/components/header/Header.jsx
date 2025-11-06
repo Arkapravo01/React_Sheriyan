7dac0787255be294fe1b929d92cf1058533d275b
Arkapravo <arkapravochakrabarti188@gmail.com>
2025-11-06 19:51:41 +0530

    Tailwind Start

---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './header.module.css'
+
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h3 className={styles.logo}>Sheriyan</h3>
+      <button className={styles.btn}>Login</button>
+    </div>
+  );
+};
+
+export default Header;
