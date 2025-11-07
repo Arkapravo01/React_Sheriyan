0fb1cf4d518df15d5f7718d8fd69c0ac1db360b0
Arkapravo <arkapravochakrabarti188@gmail.com>
2025-11-07 20:46:26 +0530

    5.12

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
-
 const App = () => {
-  function btnClicked(){
-    console.log('Hello')
-  }
   return (
     <div>
       <button>Click</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
