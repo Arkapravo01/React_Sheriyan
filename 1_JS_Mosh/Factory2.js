987ac07bfb8dfdf3517460299f6ebf5ebaa23325
Arkapravo <arkapravochakrabarti188@gmail.com>
2025-11-09 01:19:59 +0530

    64

--- 1_JS_Mosh/Factory2.js
+++ 1_JS_Mosh/Factory2.js
@@ -13,8 +13,7 @@ function Address(street, city, zipCode) {
 let address1 = new Address("a", "b", "c");
 let address2 = new Address("a", "b", "c");
 
-
-const areEqual=(address1,address2)=>{
-  return address1===address2
-}
+const areSame = (address1, address2) => {
+  return address1 === address2;
+};
 console.log(areEqual());
